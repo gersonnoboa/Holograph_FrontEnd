e6c5c8528b974b946a79b778b3b118451c65ca56
Gerson Noboa <gerson.noboa@gmail.com>
2018-03-20 20:52:28 +0200

    Deployment to Heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+//Install express server
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.all('/*', function (req, res, next) {
+    res.sendFile('/dist/index.html', { root: __dirname });
+});
+
+app.listen(process.env.PORT || 8080);
